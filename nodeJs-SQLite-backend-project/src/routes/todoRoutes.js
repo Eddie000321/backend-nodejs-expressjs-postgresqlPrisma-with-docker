d3f49566aeddf0a9181ebf81b0f587e2c1c9cda3
Eddie <Eddie000321@outlook.com>
2025-07-25 11:34:44 -0400

    creates todos post and fetch all todos get

--- nodeJs-SQLite-backend-project/src/routes/todoRoutes.js
+++ nodeJs-SQLite-backend-project/src/routes/todoRoutes.js
@@ -11,7 +11,15 @@ router.get("/", (req, res) => {
 });
 
 // create an new todo
-router.post("/", (req, res) => {});
+router.post("/", (req, res) => {
+  const { task } = req.body;
+  const insertTodo = db.prepare(
+    "INSERT INTO todos (user_id, task) VALUES(?, ?)"
+  );
+  const result = insertTodo.run(req.userId, task);
+
+  res.json({ id: result.lastInsertRowid, task, completed: 0 });
+});
 
 // update a todo
 router.put("/:id", (req, res) => {});
